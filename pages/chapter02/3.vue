608f3e1063af4a75b537a4f3e828c397be2b3420
TANABE Hiroyasu <h@cosat.com>
2022-01-30 00:21:46 +0900

    Chapter 02 3

---
+++ pages/chapter02/3.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <p>Chapter 2 2-3 いろいろな線を描く</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter02/3                                                                     */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      ctx.beginPath();
+      ctx.moveTo(100, 100);
+      ctx.lineTo(200, 100);
+      ctx.bezierCurveTo(300, 200, 100, 300, 200, 400);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(200, 500);
+      ctx.quadraticCurveTo(300, 600, 100, 700);
+      ctx.lineTo(100, 500);
+      ctx.closePath();
+      ctx.stroke();
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
