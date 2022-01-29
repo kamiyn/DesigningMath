f956f1fa3a31b602e81a5f589b0703c284c3d7f8
TANABE Hiroyasu <h@cosat.com>
2022-01-30 00:59:51 +0900

    Chapter 02 5

---
+++ pages/chapter02/5.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <p>Chapter 2 2-5 色の設定</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter02/5 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      ctx.lineWidth = 40;
+      ctx.fillStyle = "pink";
+      ctx.strokeStyle = "red";
+      ctx.beginPath();
+      ctx.arc(400, 500, 300, 0, 180 * Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.fillStyle = "rgb(160, 160, 160)";
+      ctx.strokeStyle = "#990000";
+      ctx.beginPath();
+      ctx.arc(700, 500, 300, 0, 180 * Math.PI * 2, true);
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+      ctx.beginPath();
+      ctx.arc(550, 800, 300, 0, 180 * Math.PI * 2, true);
+      ctx.fill();
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
