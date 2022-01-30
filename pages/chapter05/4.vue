47904832d3359badde5f53aa983530e6fc8dd36c
TANABE Hiroyasu <h@cosat.com>
2022-01-30 21:47:59 +0900

    Chapter 05 4

---
+++ pages/chapter05/4.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <p>Chapter 5 5-4 個々の〇の半径を時間に沿って動かす</p>
+    <input v-model="numCycle" />
+    <input v-model="numCycleRadius" />
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter05/4 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+var centerX: number;
+var centerY: number;
+var startTime: number;
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {
+    return {
+      numCycle: "8",
+      numCycleRadius: "6",
+    };
+  },
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      // スクリーン中心点を計算しておく
+      centerX = screenWidth / 2;
+      centerY = screenHeight / 2;
+      startTime = new Date().getTime(); // ミリ秒単位での UnixEpoch からの経過時間
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      const cycleSeconds = 2; // 2秒で1周したい
+      const passedTime = new Date().getTime() - startTime;
+      const objectKakudo = ((passedTime / 1000) * Math.PI * 2) / cycleSeconds;
+
+      ctx.clearRect(0, 0, screenWidth, screenHeight); // 画面の消去
+
+      const cycle = parseInt(this.numCycle, 10);
+      const cycleRadius = parseInt(this.numCycleRadius, 10);
+
+      for (var degree = 0; degree < 360; degree += 3) {
+        const kakudo = (degree / 180) * Math.PI;
+        const hankei1 = 100 * Math.sin(kakudo * cycleRadius) + 400;
+        const haba = 20;
+        /* 〇の半径に時間によって進む角度を反映 */
+        const hankei2 = haba * (Math.sin(kakudo * cycle + objectKakudo) + 1);
+
+        const x = hankei1 * Math.cos(kakudo) + centerX;
+        const y = hankei1 * Math.sin(kakudo) + centerY;
+        ctx.beginPath();
+        ctx.arc(x, y, hankei2, 0, Math.PI * 2, true);
+        ctx.stroke();
+      }
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
