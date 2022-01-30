5fc13d8719d081b8da0c987c527e83f926da70a1
TANABE Hiroyasu <h@cosat.com>
2022-01-30 21:13:12 +0900

    Chapter 05 1

---
+++ pages/chapter05/1.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <p>Chapter 5 5-1 円周上に〇を並べる</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter05/1 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+var centerX: number;
+var centerY: number;
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      // スクリーン中心点を計算しておく
+      centerX = screenWidth / 2;
+      centerY = screenHeight / 2;
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      for (var degree = 0; degree < 360; degree += 3) {
+        const kakudo = (degree / 180) * Math.PI;
+        const hankei1 = 400;
+        const hankei2 = 10;
+        const x = hankei1 * Math.cos(kakudo) + centerX;
+        const y = hankei1 * Math.sin(kakudo) + centerY;
+        ctx.beginPath();
+        ctx.arc(x, y, hankei2, 0, Math.PI * 2, true);
+        ctx.stroke();
+      }
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
