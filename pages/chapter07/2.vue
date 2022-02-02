ad4416b287511534aa460b09a6b7b148f2e87b90
TANABE Hiroyasu <h@cosat.com>
2022-02-03 00:23:39 +0900

    chapter 07 2

---
+++ pages/chapter07/2.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <p>Chapter 7 7-2 距離によって〇の大きさを変える</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter07/2 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+class Maru {
+  constructor(public x: number, public y: number) {}
+
+  // 本では事前計算を保持する変数として宣言しているが必要な時に値を計算する関数として実装している
+  public kyori(curYubiX: number, curYubiY: number): number {
+    return Math.sqrt(
+      Math.pow(curYubiX - this.x, 2) + Math.pow(curYubiY - this.y, 2)
+    );
+  }
+}
+
+const offsetX = 0;
+const offsetY = 0;
+var unitKyori = 0;
+var maruArrArr: Maru[][] = [];
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      const unitYokoKazu = 20;
+      const yokoInterval = screenWidth / (unitYokoKazu - 1);
+      const tateInterval = yokoInterval * Math.sin(Math.PI / 3);
+      const unitTateKazu = Math.ceil(screenHeight / tateInterval);
+      unitKyori = yokoInterval;
+
+      maruArrArr = [...Array(unitTateKazu).keys()].map((tateNum) =>
+        [...Array(unitYokoKazu).keys()].map((yokoNum) => {
+          const x =
+            yokoInterval * yokoNum +
+            offsetX +
+            (tateNum % 2 === 1 ? yokoInterval / 2 : 0);
+          const y = tateInterval * tateNum + offsetY;
+          return new Maru(x, y);
+        })
+      );
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      const screenSize = Math.min(screenWidth, screenHeight); // 比率計算用の画面サイズ
+      ctx.clearRect(0, 0, screenWidth, screenHeight);
+      maruArrArr.forEach((maruY) =>
+        maruY.forEach((maru) => {
+          const par = Math.min(maru.kyori(curYubiX, curYubiY) / screenSize, 1); // 1は超えない
+          const parR = 1 - par;
+          const hankei = (unitKyori / 2) * parR;
+          ctx.strokeStyle = "black";
+          ctx.lineWidth = 2;
+          ctx.beginPath();
+          ctx.arc(maru.x, maru.y, hankei, 0, Math.PI * 2, true);
+          ctx.stroke();
+        })
+      );
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
