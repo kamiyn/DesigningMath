81eb2dc39bbaaeb5f6adc291bfffa91dd594d793
TANABE Hiroyasu <h@cosat.com>
2022-02-03 01:01:19 +0900

    chapter 07 6

---
+++ pages/chapter07/6.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <p>Chapter 7 7-6 指の方向の線がゆらぐ</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter07/6 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+class Maru {
+  private touched: boolean;
+  constructor(public x: number, public y: number) {
+    this.touched = false;
+  }
+
+  // 本では事前計算を保持する変数として宣言しているが必要な時に値を計算する関数として実装している
+  public kyori(curYubiX: number, curYubiY: number): number {
+    return Math.sqrt(
+      Math.pow(curYubiX - this.x, 2) + Math.pow(curYubiY - this.y, 2)
+    );
+  }
+
+  public kakudo(curYubiX: number, curYubiY: number): number {
+    return Math.atan2(curYubiY - this.y, curYubiX - this.x);
+  }
+
+  // 本では boolean プロパティとして直接代入しているが
+  // 実プログラムではしばしば タッチ時・クリア時に別の処理をしたくなるため setter 関数として実装している
+  public setTouched(): void {
+    this.touched = true;
+  }
+  public clearTouched(): void {
+    this.touched = false;
+  }
+  public isTouched(): boolean {
+    return this.touched;
+  }
+}
+
+const offsetX = 0;
+const offsetY = 0;
+var unitKyori = 0;
+var maruArrArr: Maru[][] = [];
+var startTime: number;
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      const unitYokoKazu = 20;
+      const yokoInterval = screenWidth / (unitYokoKazu - 1);
+      const tateInterval = yokoInterval * Math.sin(Math.PI / 3);
+      const unitTateKazu = Math.ceil(screenHeight / tateInterval);
+      unitKyori = yokoInterval;
+
+      maruArrArr = [...Array(unitTateKazu).keys()].map((tateNum) =>
+        [...Array(unitYokoKazu).keys()].map((yokoNum) => {
+          const x =
+            yokoInterval * yokoNum +
+            offsetX +
+            (tateNum % 2 === 1 ? yokoInterval / 2 : 0);
+          const y = tateInterval * tateNum + offsetY;
+          return new Maru(x, y);
+        })
+      );
+      startTime = new Date().getTime(); // ミリ秒単位での UnixEpoch からの経過時間
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      const second = (new Date().getTime() - startTime) / 1000;
+      const fureKakudo = Math.sin(second * Math.PI * 2) * Math.PI * 0.2; // 1秒周期で10% = 36度ゆらぐ
+
+      const screenSize = Math.min(screenWidth, screenHeight); // 比率計算用の画面サイズ
+      ctx.clearRect(0, 0, screenWidth, screenHeight);
+      maruArrArr.forEach((maruY) =>
+        maruY.forEach((maru) => {
+          const kyori = maru.kyori(curYubiX, curYubiY);
+          const kakudo = maru.kakudo(curYubiX, curYubiY);
+          if (kyori < unitKyori / 2) {
+            maru.setTouched(); // 一度なぞられたらセットしたままにしたい。毎回プロパティセットしない
+          }
+          const parR = Math.max(1 - kyori / screenSize, 0); // 0以上にする
+          const hankei = (unitKyori / 2) * parR;
+          // 指の方向に線を描画
+          ctx.strokeStyle = "black";
+          ctx.lineWidth = parR * 5;
+          const x1 = hankei * Math.cos(kakudo + fureKakudo);
+          const y1 = hankei * Math.sin(kakudo + fureKakudo);
+          ctx.beginPath();
+          ctx.moveTo(maru.x, maru.y); // 中心に移動
+          ctx.lineTo(maru.x + x1, maru.y + y1);
+          ctx.stroke();
+
+          // 角度の描画
+          const grayValue = parR * 255;
+          ctx.fillStyle = maru.isTouched()
+            ? "red" // なぞられていたら赤くする
+            : `rgb(${grayValue},${grayValue},${grayValue})`;
+          ctx.strokeStyle = "black";
+          ctx.lineWidth = 2;
+          ctx.beginPath();
+          ctx.arc(
+            maru.x,
+            maru.y,
+            hankei,
+            kakudo - Math.PI / 2,
+            kakudo + Math.PI / 2,
+            true
+          );
+          ctx.fill();
+
+          ctx.strokeStyle = "black";
+          ctx.lineWidth = 2;
+          ctx.beginPath();
+          ctx.arc(maru.x, maru.y, hankei, 0, Math.PI * 2, true);
+          ctx.stroke();
+        })
+      );
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+      // タッチ開始時に初期化 (赤くなっていたものを戻す)
+      maruArrArr.forEach((maruY) =>
+        maruY.forEach((maru) => {
+          maru.clearTouched();
+        })
+      );
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
