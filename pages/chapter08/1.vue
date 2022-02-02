a74f0153455089e16d3f60c27dfaf3b4b9e373a1
TANABE Hiroyasu <h@cosat.com>
2022-02-03 01:07:47 +0900

    chapter 08 1

---
+++ pages/chapter08/1.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <p>Chapter 8 8-1 ●を敷き詰める</p>
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter08/1 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+class Maru {
+  constructor(public x: number, public y: number) {}
+}
+
+const offsetX = 0;
+const offsetY = 0;
+var unitKyori = 0;
+var maruArrArr: Maru[][] = [];
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {},
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+      const unitYokoKazu = 10;
+      const yokoInterval = screenWidth / (unitYokoKazu - 1);
+      const tateInterval = yokoInterval * Math.sin(Math.PI / 3);
+      const unitTateKazu = Math.ceil(screenHeight / tateInterval) + 1;
+      unitKyori = yokoInterval;
+
+      maruArrArr = [...Array(unitTateKazu).keys()].map((tateNum) =>
+        [...Array(unitYokoKazu).keys()].map((yokoNum) => {
+          const x =
+            yokoInterval * yokoNum +
+            offsetX +
+            (tateNum % 2 === 1 ? yokoInterval / 2 : 0);
+          const y = tateInterval * tateNum + offsetY;
+          return new Maru(x, y);
+        })
+      );
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      ctx.clearRect(0, 0, screenWidth, screenHeight);
+      maruArrArr.forEach((maruY) =>
+        maruY.forEach((maru) => {
+          const hankei = unitKyori / 4; // 半径は ● の距離の 1/4
+          ctx.fillStyle = "black";
+          ctx.lineWidth = 2;
+          ctx.beginPath();
+          ctx.arc(maru.x, maru.y, hankei, 0, Math.PI * 2, true);
+          ctx.fill();
+        })
+      );
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
