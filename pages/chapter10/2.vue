20c879593b95d1a9ce0ad25823f72f475eb029af
TANABE Hiroyasu <h@cosat.com>
2022-02-03 20:09:07 +0900

    chapter 10 2

---
+++ pages/chapter10/2.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <p>Chapter 10 10-2 クラスへの関数の組み込み</p>
+    <input v-model="objectDistance" />
+    <input v-model="screenDistance" />
+    <designingmath
+      :setupFunc="setupFunc"
+      :loopFunc="loopFunc"
+      :touchOrMouseStartFunc="touchOrMouseStartFunc"
+      :touchOrMouseEndFunc="touchOrMouseEndFunc"
+      :touchOrMouseMoveFunc="touchOrMouseMoveFunc"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+/* http://localhost:3000/chapter10/2 */
+import { defineComponent } from "vue";
+import designingmath from "@/components/designingmath.vue";
+
+class Choten {
+  constructor(public x: number, public y: number, public z: number) {}
+
+  // 本では直接 + 3000 している部分を引数にしている
+  gxyz(gc: number): Choten {
+    return new Choten(this.x, this.y, this.z + gc);
+  }
+
+  // 本では先に sc = 2000 としている部分を引数にしている
+  tx(gc: number, sc: number): number {
+    const g = this.gxyz(gc);
+    return (sc * g.x) / g.z;
+  }
+
+  ty(gc: number, sc: number): number {
+    const g = this.gxyz(gc);
+    return (sc * g.y) / g.z;
+  }
+}
+
+const henLength = 400;
+var choten: Choten[] = [
+  new Choten(henLength, henLength, henLength),
+  new Choten(henLength, henLength, -henLength),
+  new Choten(-henLength, henLength, -henLength),
+  new Choten(-henLength, henLength, henLength),
+  new Choten(henLength, -henLength, henLength),
+  new Choten(henLength, -henLength, -henLength),
+  new Choten(-henLength, -henLength, -henLength),
+  new Choten(-henLength, -henLength, henLength),
+];
+
+export default defineComponent({
+  components: {
+    designingmath,
+  },
+  setup() {
+    return {
+      objectDistance: "3000", // 物体までの距離
+      screenDistance: "2000", // スクリーンまでの距離
+    };
+  },
+  methods: {
+    setupFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("setupFunc");
+    },
+    loopFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("loopFunc");
+      const gc = parseInt(this.objectDistance);
+      const sc = parseInt(this.screenDistance); 
+
+      ctx.clearRect(0, 0, screenWidth, screenHeight);
+
+      ctx.save();
+      ctx.translate(screenWidth / 2, screenHeight / 2); // 原点を画面キャンバス中央に移動
+
+      ctx.lineWidth = 2;
+      ctx.lineJoin = "round";
+
+      ctx.strokeStyle = "black";
+      ctx.beginPath();
+      ctx.moveTo(choten[0].tx(gc, sc), choten[0].ty(gc, sc));
+      ctx.lineTo(choten[1].tx(gc, sc), choten[1].ty(gc, sc));
+      ctx.lineTo(choten[2].tx(gc, sc), choten[2].ty(gc, sc));
+      ctx.lineTo(choten[3].tx(gc, sc), choten[3].ty(gc, sc));
+      ctx.lineTo(choten[0].tx(gc, sc), choten[0].ty(gc, sc));
+      ctx.lineTo(choten[4].tx(gc, sc), choten[4].ty(gc, sc));
+      ctx.lineTo(choten[5].tx(gc, sc), choten[5].ty(gc, sc));
+      ctx.lineTo(choten[6].tx(gc, sc), choten[6].ty(gc, sc));
+      ctx.lineTo(choten[7].tx(gc, sc), choten[7].ty(gc, sc));
+      ctx.lineTo(choten[4].tx(gc, sc), choten[4].ty(gc, sc));
+      ctx.moveTo(choten[1].tx(gc, sc), choten[1].ty(gc, sc));
+      ctx.lineTo(choten[5].tx(gc, sc), choten[5].ty(gc, sc));
+      ctx.moveTo(choten[2].tx(gc, sc), choten[2].ty(gc, sc));
+      ctx.lineTo(choten[6].tx(gc, sc), choten[6].ty(gc, sc));
+      ctx.moveTo(choten[3].tx(gc, sc), choten[3].ty(gc, sc));
+      ctx.lineTo(choten[7].tx(gc, sc), choten[7].ty(gc, sc));
+      ctx.stroke();
+
+      ctx.restore();
+    },
+    touchOrMouseStartFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseStartFunc");
+    },
+    touchOrMouseMoveFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseMoveFunc");
+    },
+    touchOrMouseEndFunc(
+      ctx: CanvasRenderingContext2D,
+      screenWidth: number,
+      screenHeight: number,
+      curYubiX: number,
+      curYubiY: number,
+      yubiTouched: boolean
+    ) {
+      console.log("touchOrMouseEndFunc");
+    },
+  },
+});
+</script>
