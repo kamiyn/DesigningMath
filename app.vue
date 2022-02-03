e61fec37a5f87508006f8d4b88fc1115ccbe5741
TANABE Hiroyasu <h@cosat.com>
2022-02-04 00:08:12 +0900

    menu の追加

--- app.vue
+++ app.vue
@@ -1,5 +1,208 @@
 <template>
   <div>
     <NuxtPage />
+    <div class="mainmenu">
+      <input type="checkbox" id="menu" />
+      <div class="menu">
+        <label class="menu-toggle" for="menu">
+          <font-awesome-icon icon="bars" />
+        </label>
+        <ul>
+          <li>
+            <label for="menu-c01">Chapter 01</label>
+            <input type="checkbox" id="menu-c01" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter01/0">01-0</NuxtLink></li>
+                <li><NuxtLink to="/chapter01/1">01-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter01/2">01-2</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c02">Chapter 02</label>
+            <input type="checkbox" id="menu-c02" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter02/1">02-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/2">02-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/3">02-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/4">02-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/5">02-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/6">02-6</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/7">02-7</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/8">02-8</NuxtLink></li>
+                <li><NuxtLink to="/chapter02/9">02-9</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c03">Chapter 03</label>
+            <input type="checkbox" id="menu-c03" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter03/1">03-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter03/2">03-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter03/3">03-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter03/4">03-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter03/5">03-5</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c04">Chapter 04</label>
+            <input type="checkbox" id="menu-c04" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter04/1">04-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/2">04-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/3">04-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/4">04-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/5">04-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/6">04-6</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/7">04-7</NuxtLink></li>
+                <li><NuxtLink to="/chapter04/8">04-8</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c05">Chapter 05</label>
+            <input type="checkbox" id="menu-c05" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter05/1">05-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter05/2">05-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter05/3">05-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter05/4">05-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter05/5">05-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter05/6">05-6</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c06">Chapter 06</label>
+            <input type="checkbox" id="menu-c06" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter06/1">06-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter06/2">06-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter06/3">06-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter06/4">06-4</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c07">Chapter 07</label>
+            <input type="checkbox" id="menu-c07" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter07/1">07-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter07/2">07-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter07/3">07-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter07/4">07-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter07/5">07-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter07/6">07-6</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c08">Chapter 08</label>
+            <input type="checkbox" id="menu-c08" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter08/1">08-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/2">08-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/3">08-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/4">08-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/5">08-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/6">08-6</NuxtLink></li>
+                <li><NuxtLink to="/chapter08/7">08-7</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c09">Chapter 09</label>
+            <input type="checkbox" id="menu-c09" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter09/1">09-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter09/2">09-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter09/3">09-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter09/4">09-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter09/5">09-5</NuxtLink></li>
+                <li><NuxtLink to="/chapter09/6">09-6</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <label for="menu-c10">Chapter 10</label>
+            <input type="checkbox" id="menu-c10" />
+            <div class="menu">
+              <ul>
+                <li><NuxtLink to="/chapter10/1">10-1</NuxtLink></li>
+                <li><NuxtLink to="/chapter10/2">10-2</NuxtLink></li>
+                <li><NuxtLink to="/chapter10/3">10-3</NuxtLink></li>
+                <li><NuxtLink to="/chapter10/4">10-4</NuxtLink></li>
+                <li><NuxtLink to="/chapter10/5">10-5</NuxtLink></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faBars);
+
+export default defineComponent({
+  components: {
+    FontAwesomeIcon,
+  },
+});
+</script>
+
+<style lang="sass">
+label
+  cursor: pointer
+  &:focus
+    outline: none
+
+.menu
+  position: absolute
+  top: 0
+  left: 0
+  background: #fff
+  width: 240px
+  height: 100%
+  transform: translate3d(-240px, 0, 0)
+  transition: transform 0.1s
+
+  label.menu-toggle
+    position: absolute
+    right: -60px
+    width: 60px
+    height: 60px
+    margin: 5px
+    display: block
+
+  ul
+    li
+      list-style-type: none
+
+.mainmenu input[type=checkbox]
+  display: none
+
+.menu .menu label.menu-toggle
+  background: none
+
+.mainmenu input[type=checkbox]:checked + .menu
+  transform: translate3d(0, 0, 0)
+</style>
